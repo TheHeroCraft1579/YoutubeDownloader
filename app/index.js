13c72ed9c4f83bc7c1e4a7d7755f246d9473aea4
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2020-09-13 17:14:52 +0200

    Updated

--- app/index.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-
-  // SUBMIT FORM
-    $("#download-form").submit(function(event) {
-    // Prevent the form from submitting via the browser.
-    event.preventDefault();
-  });
